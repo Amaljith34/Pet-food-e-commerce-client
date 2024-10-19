b78c596708f4d861d23c4a9b916846610618f86e
Amaljith-aj <jith51541@gmail.com>
2024-10-19 15:38:55 +0530

    apiUpdate

---
+++ src/User/Pages/Adresspage/Adresspage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Adresspage = () => {
+  return (
+    <>
+    <h1>Adress</h1>
+    <form action="">
+        <label htmlFor=""><input type="text" /></label>
+    </form>
+    </>
+  )
+}
+
+export default Adresspage
