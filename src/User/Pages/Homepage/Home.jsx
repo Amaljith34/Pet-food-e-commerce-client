566cb59d191acd59bef82662db059bfeeb33283a
AMALJITH K <156279690+Amaljith34@users.noreply.github.com>
2024-10-29 15:43:10 +0530

    Update Home.jsx

--- src/User/Pages/Homepage/Home.jsx
+++ src/User/Pages/Homepage/Home.jsx
@@ -8,7 +8,7 @@ export default function HomePage() {
         <div className="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
           <div className="sm:max-w-lg">
             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-            Welcome to Petpal.in
+            Welcome to PetLuxe.in
             </h1>
             <p className="mt-4 text-xl text-gray-500">
             Explore our wide range of premium pet foods and accessories. From
